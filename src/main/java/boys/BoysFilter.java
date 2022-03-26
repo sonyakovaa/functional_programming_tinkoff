9c2178242851867befd4f9af69bbcf30bcd76dfe
Sofya Kovaleva <s.kovaleva@profsoft.pro>
2022-03-26 14:56:28 +0400

    Добавлено решение задания 10.3

--- src/main/java/boys/BoysFilter.java
+++ src/main/java/boys/BoysFilter.java
@@ -2,10 +2,19 @@ package boys;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class BoysFilter {
 
     public static Map<String, Long> getFilteredBoys(ArrayList<Boy> boys) {
-        // TODO
+        return boys.stream()
+                .filter(boy -> boy.getAge() >= 18)
+                .map(Boy::getName)
+                .distinct()
+                .sorted()
+                .limit(4)
+                .collect(Collectors.toMap(Function.identity(), name -> boys.stream()
+                        .filter(boy -> boy.getName().equals(name)).count() - 1));
     }
 }
